699499d2cabd6126854ee24172942cf3f2b1ea96
sycho <whtnsduf0102@naver.com>
2022-08-02 17:00:27 +0900

    08/02 lection 1st

--- cookie2.js
+++ cookie2.js
@@ -10,27 +10,24 @@ const parseCookies = (cookie = '') =>
     .reduce((acc, [k, v]) => {
         acc[k.trim()] = decodeURIComponent(v);
         return acc;
-    }, {});
+    }, {}); //req.headers.cookie 문자열을 객체로 바꿔주는 함수
 
 http.createServer(async (req, res) => {
     const cookies = parseCookies(req.headers.cookie); // { mycookie: 'test' }
-    // 주소가 /login으로 시작하는 경우
-    if (req.url.startsWith('/login')) {
+    if (req.url.startsWith('/login')) {  // 주소가 /login으로 시작하는 경우
         const { query } = url.parse(req.url);
         const { name } = qs.parse(query);
         const expires = new Date();
-        // 쿠키 유효 시간을 현재시간 + 5분으로 설정
-        expires.setMinutes(expires.getMinutes() + 5);
-        res.writeHead(302, {
+        expires.setMinutes(expires.getMinutes() + 5);// 쿠키 유효 시간을 현재시간 + 5분으로 설정
+        res.writeHead(302, { //redirect
             Location: '/',
-            'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
-        });
+            'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`, 
+        }); //HttpOnly: 자바스크립트로 접근하지못하도록, Path=/ : / 아래에 있는 것들은 쿠키가 다 유효하다.
         res.end();
-        // name이라는 쿠키가 있는 경우
-    } else if (cookies.name) {
+    } else if (cookies.name) {  // name이라는 쿠키가 있는 경우
         res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end(`${cookies.name}님 안녕하세요`);
-    } else {
+    } else { //쿠키가 없을때
         try {
             const data = await fs.readFile('./cookie2.html');
             res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
---
+++ express/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path'); //경로처리
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    // res.send('Hello, Express');
+    res.sendFile(path.join(__dirname, '/index.html')); //path 모듈을 사용
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
